9c54c00b299961599fb0c1ce0ec9d8ee4c354a38
Rashmi Ranjan Parija <rashmiofficial45@gmail.com>
2024-01-06 20:16:43 +0530

    Successfully connected to MONGO ATLAS

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constants"
+// import express from "express"
+// const app = express()
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+dotenv.config({
+    path:"./env"
+})
+
+connectDB();
+
+
+
+/*
+;( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("ERROR:",error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on PORT:${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR:",error);
+        throw error
+    }
+})()
+*/
