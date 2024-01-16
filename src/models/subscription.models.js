9359c504f708f6fa6c02c14a60f973f78011f374
Rashmi Ranjan Parija <rashmiofficial45@gmail.com>
2024-01-16 14:09:54 +0530

    subscription model added

---
+++ src/models/subscription.models.js
@@ -0,0 +1,14 @@
+import mongoose , {Schema} from "mongoose"
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId,
+        ref:"User"   //One who is subscribing
+    },
+    channel:{
+        type:Schema.Types.ObjectId,
+        ref:"User"   //One to whom the subscriber is subscribing
+    },
+},{timestamps:true})
+
+export const Subscription = mongoose.model("Subscription",subscriptionSchema)
